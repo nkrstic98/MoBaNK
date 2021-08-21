903411dc6bd2019cf3dc9c2d9d22073698eb9822
nkrstic98 <nikolakrle998@gmail.com>
2021-08-21 17:56:11 +0200

    Added basic Hilt dependencies and enabled Internet use in the app

---
+++ app/src/main/java/rs/ac/bg/etf/diplomski/authenticationapp/AuthenticationApplication.java
@@ -0,0 +1,8 @@
+package rs.ac.bg.etf.diplomski.authenticationapp;
+
+import android.app.Application;
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class AuthenticationApplication extends Application {
+}
