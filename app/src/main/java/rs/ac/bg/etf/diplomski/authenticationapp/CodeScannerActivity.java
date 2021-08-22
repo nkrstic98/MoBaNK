f6714f5e0845345e75bfe5bfe3b344e0dce28abd
nkrstic98 <nikolakrle998@gmail.com>
2021-08-22 18:28:59 +0200

    Code scanning and SMS verification implemented

--- app/src/main/java/rs/ac/bg/etf/diplomski/authenticationapp/CodeScannerActivity.java
+++ app/src/main/java/rs/ac/bg/etf/diplomski/authenticationapp/CodeScannerActivity.java
@@ -6,10 +6,8 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -21,8 +19,12 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import dagger.hilt.android.AndroidEntryPoint;
 import rs.ac.bg.etf.diplomski.authenticationapp.databinding.ActivityCodeScannerBinding;
@@ -37,6 +39,7 @@ public class CodeScannerActivity extends AppCompatActivity {
 
     private ActivityCodeScannerBinding binding;
     private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
     private CodeScanner codeScanner;
 
     private boolean phone_verification_in_progress = false;
@@ -50,48 +53,17 @@ public class CodeScannerActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
 
         setupCameraPermissions();
         initCodeScanner();
-    }
-
-    private void initCodeScanner() {
-        codeScanner = new CodeScanner(this, binding.scannerView);
-
-        codeScanner.setCamera(CodeScanner.CAMERA_BACK);
-        codeScanner.setFormats(CodeScanner.ALL_FORMATS);
-        codeScanner.setAutoFocusMode(AutoFocusMode.SAFE);
-        codeScanner.setScanMode(ScanMode.CONTINUOUS);
-        codeScanner.setAutoFocusEnabled(true);
-        codeScanner.setFlashEnabled(false);
-
-        codeScanner.setDecodeCallback(result -> {
-            try {
-                String[] scanned_data = result.getText().split("@");
-
-                verifyPhoneNumber(scanned_data[1]);
-            }
-            catch (Exception e) {
-                Toast.makeText(this, "Invalid QR Code. Please try again", Toast.LENGTH_SHORT);
-            }
-        });
-
-        codeScanner.setErrorCallback(error -> {
-            runOnUiThread(() -> {
-                Log.e("scanner-error", "Camera initialization error" + error.getMessage());
-            });
-        });
-
-        binding.scannerView.setOnClickListener(v -> {
-            codeScanner.startPreview();
-        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         if(phone_verification_in_progress && phone_number != "") {
-            //verifyPhoneNumber(phone_number);
+            verifyPhoneNumber(phone_number);
         }
     }
 
@@ -119,6 +91,60 @@ public class CodeScannerActivity extends AppCompatActivity {
         super.onRestoreInstanceState(savedInstanceState);
         phone_verification_in_progress = savedInstanceState.getBoolean(PHONE_VERIFICATION_SIS_KEY);
         phone_number = savedInstanceState.getString(PHONE_VERIFICATION_SIS_NUMBER, "");
+    }
+
+    private void initCodeScanner() {
+        codeScanner = new CodeScanner(this, binding.scannerView);
+
+        codeScanner.setCamera(CodeScanner.CAMERA_BACK);
+        codeScanner.setFormats(CodeScanner.ALL_FORMATS);
+        codeScanner.setAutoFocusMode(AutoFocusMode.SAFE);
+        codeScanner.setScanMode(ScanMode.SINGLE);
+        codeScanner.setAutoFocusEnabled(true);
+        codeScanner.setFlashEnabled(false);
+
+        codeScanner.setDecodeCallback(result -> {
+            try {
+                String[] scanned_data = result.getText().split("@");
+
+                verifyUserKey(scanned_data[0], scanned_data[1], scanned_data[2]);
+            }
+            catch (Exception e) {
+                runOnUiThread(() -> {
+                    Toast.makeText(this, "Invalid QR Code. Please try again", Toast.LENGTH_SHORT).show();
+                });
+            }
+        });
+
+        codeScanner.setErrorCallback(error -> {
+            runOnUiThread(() -> {
+                Log.e("scanner-error", "Camera initialization error" + error.getMessage());
+            });
+        });
+
+        binding.scannerView.setOnClickListener(v -> {
+            codeScanner.startPreview();
+        });
+    }
+
+    private void verifyUserKey(String id, String key, String phoneNumber) {
+        firebaseFirestore
+                .collection("users")
+                .whereEqualTo("id", id)
+                .get()
+                .addOnSuccessListener(command -> {
+                    List<DocumentSnapshot> documentSnapshotList = command.getDocuments();
+                    if(documentSnapshotList.size() != 0 && documentSnapshotList.get(0).getId().equals(key)) {
+                        verifyPhoneNumber(phoneNumber);
+                    }
+                    else {
+                        Toast.makeText(this, "Invalid QR code! Use different code, or contact QR Code provider.", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    Log.e("firebase-tag", e.getMessage());
+                    Toast.makeText(this, "Something went wrong. Please try again later!", Toast.LENGTH_SHORT).show();
+                });
     }
 
     private void verifyPhoneNumber(String phoneNumber) {
@@ -152,8 +178,6 @@ public class CodeScannerActivity extends AppCompatActivity {
                                 super.onCodeAutoRetrievalTimeOut(s);
                                 Log.d("verification-tag", "onCodeAutoRetrievalTimeOut: " + s);
                             }
-
-
                         })
                         .build();
 
@@ -177,7 +201,7 @@ public class CodeScannerActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == CAMERA_REQUEST_CODE) {
             if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "You need camera permission to be able to use this app", Toast.LENGTH_SHORT);
+                Toast.makeText(this, "You need camera permission to be able to use this app", Toast.LENGTH_SHORT).show();
             }
         }
     }
---
+++ app/src/main/java/rs/ac/bg/etf/diplomski/authenticationapp/models/User.java
@@ -0,0 +1,71 @@
+package rs.ac.bg.etf.diplomski.authenticationapp.models;
+
+public class User {
+    private String id = "";
+    private String firstname = "";
+    private String lastname = "";
+    private String email = "";
+    private String phone = "";
+    private String gender = "";
+
+    public User() {
+
+    }
+
+    public User(String id, String firstname, String lastname, String email, String phone, String gender) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.phone = phone;
+        this.gender = gender;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
