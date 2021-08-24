c2d6db23d021cbb37b2dae163f75c805f6da5174
nkrstic98 <nikolakrle998@gmail.com>
2021-08-24 15:49:55 +0200

    User signing out implemented when activity is not in focus

--- app/src/main/java/rs/ac/bg/etf/diplomski/authenticationapp/MainActivity.java
+++ app/src/main/java/rs/ac/bg/etf/diplomski/authenticationapp/MainActivity.java
@@ -2,7 +2,12 @@ package rs.ac.bg.etf.diplomski.authenticationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -12,5 +17,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        returnToLogin();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        logout();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        logout();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        logout();
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+    }
+
+    private void returnToLogin() {
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
